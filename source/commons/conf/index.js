ebd568960976485490608e99af273590977483c8
renato.vicente <rntvicente@gmail.com>
2018-07-29 22:22:04 -0300

    Configurações

--- source/commons/conf/index.js
+++ source/commons/conf/index.js
@@ -4,8 +4,7 @@ nconf.argv()
   .env()
   .file('config/store-curso-config.json')
   .defaults({
-    PORT: 3000,
-    MONGO_URL: 'mongodb://127.0.0.1/store-nodejs' 
+    PORT: 3000
   });
 
 module.exports = nconf;
